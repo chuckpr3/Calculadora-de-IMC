f3018efd089f82b9d5a561a6a4ec269365c511d8
Claudinei Sampaio <jmihawk11@gmail.com>
2022-04-10 13:36:22 -0300

    Calculadora-de-IMC

---
+++ aula 14/script.js
@@ -0,0 +1,42 @@
+function meuEscopo() {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+    
+
+        function recebeEventoForm(evento){
+        evento.preventDefault(); 
+
+        const peso = form.querySelector('.peso').value;
+        const altura = form.querySelector('.altura').value;
+        const imc = peso / (altura * altura);
+    
+        resultado.innerHTML += `Seu IMC é ${imc.toFixed(2)}`
+        if (imc <= 18.5) {
+            resultado.innerHTML += ` (Abaixo do peso)`
+
+        } else if(imc >= 18.5 && imc <= 24.9) {
+            resultado.innerHTML += ` (Peso normal)`
+
+        } else if(imc >= 25 && imc <= 29.9) {
+            resultado.innerHTML += ` (Sobrepeso)`
+
+        }else if(imc >= 30 && imc <= 34.9) {
+            resultado.innerHTML += ` (Obesidade grau 1)`
+
+        }else if(imc >= 35 && imc <= 39.9) {
+            resultado.innerHTML += ` (Obesidade grau 2)`
+
+        }else if(imc >= 40) {
+            resultado.innerHTML += ` (Obesidade grau 3)`
+
+        }else {
+            resultado.innerHTML += ` (ERRO)`
+        }
+        
+    }
+    form.addEventListener('submit', recebeEventoForm);
+
+
+
+}
+meuEscopo();
